15d9a550f4db247e8e67f4987712531d6ee86b03
Lolo179 <luismanolo92@gmail.com>
2022-04-15 21:34:05 +0200

    Comit inicial con eclipse. Close #1

---
+++ src/main/java/com/AiCapDesigns/springboot/app/AiCapDesignsAppApplication.java
@@ -0,0 +1,13 @@
+package com.AiCapDesigns.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCapDesignsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCapDesignsAppApplication.class, args);
+	}
+
+}
