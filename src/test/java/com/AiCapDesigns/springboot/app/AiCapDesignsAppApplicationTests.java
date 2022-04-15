e1709154f49dc51d6c47ecb6954ae75da2ace810
Lolo179 <luismanolo92@gmail.com>
2022-04-15 21:34:05 +0200

    Comit inicial con eclipse

---
+++ src/test/java/com/AiCapDesigns/springboot/app/AiCapDesignsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AiCapDesigns.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCapDesignsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
